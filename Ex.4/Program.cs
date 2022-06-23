53b46d30e288adbb4a4e8f6e014b89595804764a
Arseniy <aben@yandex.ru>
2022-06-24 01:25:16 +0300

    Сделана 4 задача

--- Ex.4/Program.cs
+++ Ex.4/Program.cs
@@ -4,3 +4,22 @@
 44 5 78 -> 78
 22 3 9 -> 22
 */
+
+Console.WriteLine("введите первое число");
+string first = Console.ReadLine();
+int firstnumber = int.Parse(first);
+
+Console.WriteLine("введите второе число");
+string second = Console.ReadLine();
+int secondnumber = int.Parse(second);
+
+Console.WriteLine("введите третье число");
+string third = Console.ReadLine();
+int thirdnumber = int.Parse(third);
+
+int max = firstnumber;
+
+if (max < secondnumber) max = secondnumber;
+if (max < thirdnumber) max = thirdnumber;
+
+Console.WriteLine ($"Максимальное число {max}");
