482bc59e0771b50c4de3a9d21d8a0ffdef95eec3
Arseniy <aben@yandex.ru>
2022-06-24 02:35:39 +0300

    Сделана 8 задача

--- Ex.8/Program.cs
+++ Ex.8/Program.cs
@@ -3,3 +3,15 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= number)
+{
+    if (i%2==0)
+    {
+    Console.Write($"{i} ");
+    }
+    i++;
+}
