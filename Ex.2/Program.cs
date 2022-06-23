ec2c577ead53df03e64fdadefa1066d8cdf12694
Arseniy <aben@yandex.ru>
2022-06-24 00:59:04 +0300

    Сделана 2 задача

--- Ex.2/Program.cs
+++ Ex.2/Program.cs
@@ -4,3 +4,24 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
+
+Console.WriteLine("введите первое число");
+string first = Console.ReadLine();
+int firstnumber = int.Parse(first);
+
+Console.WriteLine("введите второе число");
+string second = Console.ReadLine();
+int secondnumber = int.Parse(second);
+
+if (firstnumber == secondnumber)
+{
+    Console.WriteLine("числа равны");
+}
+else if (firstnumber > secondnumber)
+{
+    Console.WriteLine($"max {firstnumber}");
+}
+else if (firstnumber < secondnumber)
+{
+    Console.WriteLine($"max {secondnumber}");
+}
